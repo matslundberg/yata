2da12a4e3eae00b92a3fa64d371dd6013a4fa15e
Mats Lundberg <mats.lundberg@paf.com>
2018-08-04 15:45:51 +0300

    Added tests for parseCommand

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+	"strings"
+	"github.com/stretchr/testify/assert"
+	"fmt"
+)
+
+
+func TestParseCommand(t *testing.T) {
+	args := ""
+	assert := assert.New(t)
+
+	args = "list tasks status is open"
+	command, data, filter, err := parseCommand(strings.Split(args, " "))
+	assert.Nil(err, fmt.Sprintf("parseCommand failed for %s", args))
+	assert.Equal(command, "list", "parseCommand failed to parse command")
+	assert.Equal(data, "tasks", "parseCommand failed to parse data")
+	assert.Equal(len(filter), 3, "parseCommand failed to parse filter")
+
+	args = "list tasks +tag"
+	command, data, filter, err = parseCommand(strings.Split(args, " "))
+	assert.Nil(err, fmt.Sprintf("parseCommand failed for %s", args))
+	assert.Equal(command, "list", "parseCommand failed to parse command")
+	assert.Equal(data, "tasks", "parseCommand failed to parse data")
+	assert.Equal(len(filter), 1, "parseCommand failed to parse filter")
+
+	args = "list tasks @mention"
+	command, data, filter, err = parseCommand(strings.Split(args, " "))
+	assert.Nil(err, fmt.Sprintf("parseCommand failed for %s", args))
+	assert.Equal(command, "list", "parseCommand failed to parse command")
+	assert.Equal(data, "tasks", "parseCommand failed to parse data")
+	assert.Equal(len(filter), 1, "parseCommand failed to parse filter")
+}
